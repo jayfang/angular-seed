767cbecaddce30391520f8e9674c015c5419188c
Jason Alcock <jason.alcock@sage.com>
2013-07-30 09:47:36 +0100

    Inherited version of deck controllers

--- app/js/services.js
+++ app/js/services.js
@@ -6,6 +6,6 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', { major:'0.1', build:'b123456'} )
+  value('version', { major:'0.11', build:'b123456'} )
   .value('roomState', { peopleInRoom: ['Jim', 'Jean', 'John'], iAm: 'John' });
   
--- devenv/js/playerDeckControllers.js
+++ devenv/js/playerDeckControllers.js
@@ -2,24 +2,26 @@
 
 /* Controllers */
 
-angular.module('playerdeck.controllers', []).
-// Application Test Controllers
-  controller('deck', ['$scope', 'roomState', function(sc, rs) {  
+/* //JDA - Do I want to reduce scope of visibility of baseFunc */
+var baseFunc, preset;
+
+baseFunc = function(sc, rs) {  
   	sc.deck = [1, 2, 4, 8, 16, 32, 64];
 		sc.$on( 'vote', function(event, newVote) {
   		sc.voteIndex = newVote;
-    	// sc.present.push('Person' + (sc.present.length + 1)  );
   	});
   	
   	sc.voteIndex = undefined;
-  	
-  	
-  }])
-  .controller('deckVoted1st', ['$scope', 'roomState', function(sc, rs) {
-		sc.$on( 'vote', function(event, newVote) {
-  		sc.voteIndex = newVote;
-    	// sc.present.push('Person' + (sc.present.length + 1)  );
-  	});
-  	sc.deck = [1, 2, 4, 8, 16, 32, 64];
-  	sc.voteIndex = 2;
-  }]);
+};
+  
+//JDA want to clone baseFunc but set voteIndex = 0
+preset = function(sc, rs) {
+	baseFunc(sc, rs);
+ 	sc.voteIndex = 0;
+}
+
+
+angular.module('playerdeck.controllers', []).
+// Application Test Controllers
+  controller('deck', ['$scope', 'roomState', baseFunc])
+  .controller('deckVoted1st', ['$scope', 'roomState', preset]);
